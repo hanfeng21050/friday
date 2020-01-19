c5bd0887405b5b7228f24025a622dc1c98a55e5d
hanfeng <hanfeng21050@163.com>
2020-01-19 12:05:33 +0800

    数据源监控功能 druid  解决sql监控和spring监控无数据问题

--- src/main/java/com/hf/friday/FridayApplication.java
+++ src/main/java/com/hf/friday/FridayApplication.java
@@ -2,9 +2,11 @@ package com.hf.friday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@ImportResource(locations = { "classpath:druid-bean.xml" })
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class FridayApplication {
 
